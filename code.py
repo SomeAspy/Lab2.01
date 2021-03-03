fc190cd0575f382f5088b82946cb509bbab003ec
Aiden <33640860+SomeAspy@users.noreply.github.com>
2021-03-03 12:10:06 -0500

    fixxy thingy
    Signed-off-by: Aiden <33640860+SomeAspy@users.noreply.github.com>

--- code.py
+++ code.py
@@ -1,12 +1,12 @@
 type = input("enter int for integer output, or flt for float output.\n")
 if type=="int":
     try:    
-        print("\n"+int(int(input("Number\n"))/2))
+        print("\n"+str(int(int(input("Number\n"))/2)))
     except:
         print("\nYou broke it.")
 elif type=="flt":
     try:
-        print("\n"+float(int(input("Number\n"))/2))
+        print("\n"+str(float(int(input("Number\n"))/2)))
     except:
         print("\nYou broke it.")
 else:
